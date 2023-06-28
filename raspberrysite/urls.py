721ae62b6578a1a503e90678508cec9dc468249b
balasiddhartha <balasiddhartha>
2023-06-29 03:22:28 +0530

    URL Goof up fixed

--- raspberrysite/urls.py
+++ raspberrysite/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("/commands", include("commands.urls")),
-
+    path("", include("commands.urls")),
 ]
