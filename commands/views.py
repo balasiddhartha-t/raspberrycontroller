848662182d952866c7db14a31750ea508f6d5f10
Bala Siddhartha <bala.siddhartha@gmail.com>
2023-07-13 00:28:13 +0000

    Fixing some configurations and adding MinIO as a datastore

--- commands/views.py
+++ commands/views.py
@@ -2,16 +2,35 @@ from django.http import HttpResponse
 import RPi.GPIO as GPIO
 import cv2
 import os
+from minio import Minio
+from datetime import datetime
+import ssl
+import asyncio
+from asgiref.sync import async_to_sync
+from dotenv import load_dotenv
+import pathlib
 
+load_dotenv()
+
+
+videolocation =os.environ.get('VIDEO_PATH') + os.environ.get('VIDEO_FILENAME') 
 video = cv2.VideoCapture(0)
 frame_width = int(video.get(3))
 frame_height = int(video.get(4))
 size = (frame_width, frame_height)
-result = cv2.VideoWriter(os.getcwd() + 'myvideo.avi', 
-                         cv2.VideoWriter_fourcc(*'MJPG'),
-                         10, size)
+fps = int(video.get(cv2.CAP_PROP_FPS))
+output = cv2.VideoWriter(videolocation, 
+                         cv2.VideoWriter_fourcc(*'MP4V'),
+                         fps, size)
 
 
+client = Minio(
+    os.environ.get('MINIO_CLIENT_IP'),
+    access_key=os.environ.get('MINIO_ACCESS_KEY'),
+    secret_key=os.environ.get('MINIO_SECRET_KEY'),
+    secure=False,
+    region=os.environ.get('MINIO_REGION')
+)
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.OUT)
@@ -22,14 +41,35 @@ GPIO.setup(10, GPIO.OUT)
 def index(request):
     return HttpResponse("Hello, world. You're at the commands index. Please choose from forward/backward/right/left/stop")
 
+async def recordVideo():
+    print("wait here")
+
+
 def record(request):
-    ret, frame = video.read()
-    result.write(frame)
+    print('Starting recording')
+    print(videolocation)
+    while (video.isOpened()):
+        print("recording video here")
+        ret, frame = video.read()
+        if not ret:
+            output.release()
+        output.write(frame)
     return HttpResponse("Started Recording")
 
 def stoprecord(request):
     video.release()
-    result.release()
+    output.release()
+    found = client.bucket_exists("raspi-bucket")
+    print('Starting Upload')
+    if not found:
+        return HttpResponse("Issue with uploading. Bucket not found")
+    now = datetime.now()
+    current_time = now.strftime("%H-%M-%S")
+    print(videolocation)
+    client.fput_object(
+        "raspi-bucket", "record" + current_time+".mp4", videolocation,
+    )
+    print('Finished Uploading')
     return HttpResponse("Stopped recording")
 
 def command(request, command):
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,3 +4,9 @@ apt-get install -y python3 cmake python3-pip python3-django python3-dev python3-
 dpkg --configure -a
 apt autoremove -y
 pip install -r requirements.txt
+mkdir /temp
+cp /home/kali/Desktop/raspberrycontroller/raspidjango.service /lib/systemd/system/raspidjango.service
+systemctl daemon-reload
+systemctl start raspidjango.service
+systemctl status raspidjango.service
+journalctl -f -u raspidjango
