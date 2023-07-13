8d82c0e4b32bdafba2623b68581b85552e2411d7
Bala Siddhartha <bala.siddhartha@gmail.com>
2023-07-13 01:59:02 +0000

    Cleaned up some unused packages, Used threads

--- commands/urls.py
+++ commands/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("record/start", views.record, name="recordstart"),
     path("record/stop", views.stoprecord, name="stoprecord"),
-    path("<str:command>", views.command, name="command"),
+    path("command/<str:command>", views.command, name="command"),
 ]
--- commands/views.py
+++ commands/views.py
@@ -4,24 +4,31 @@ import cv2
 import os
 from minio import Minio
 from datetime import datetime
-import ssl
-import asyncio
-from asgiref.sync import async_to_sync
 from dotenv import load_dotenv
-import pathlib
+import threading
 
 load_dotenv()
 
+def get_video_indices():
+    devs = os.listdir('/dev')
+    vid_indices = [int(dev[-1]) for dev in devs 
+            if dev.startswith('video')]
+    vid_indices = sorted(vid_indices)
+    return vid_indices
 
-videolocation =os.environ.get('VIDEO_PATH') + os.environ.get('VIDEO_FILENAME') 
-video = cv2.VideoCapture(0)
+
+
+vid_indices = get_video_indices()
+videoname = os.environ.get('VIDEO_FILENAME') +".mp4"
+videolocation = os.environ.get('VIDEO_PATH') + os.environ.get('VIDEO_FILENAME')
+video = cv2.VideoCapture(vid_indices[0])
 frame_width = int(video.get(3))
 frame_height = int(video.get(4))
 size = (frame_width, frame_height)
 fps = int(video.get(cv2.CAP_PROP_FPS))
 output = cv2.VideoWriter(videolocation, 
                          cv2.VideoWriter_fourcc(*'MP4V'),
-                         fps, size)
+                         fps, size,)
 
 
 client = Minio(
@@ -41,36 +48,53 @@ GPIO.setup(10, GPIO.OUT)
 def index(request):
     return HttpResponse("Hello, world. You're at the commands index. Please choose from forward/backward/right/left/stop")
 
-async def recordVideo():
-    print("wait here")
 
+def background_record(request, videolocation=videolocation):
+    vid_indices = get_video_indices()
+    global video
+    video = cv2.VideoCapture(vid_indices[0])
 
-def record(request):
-    print('Starting recording')
-    print(videolocation)
+    output = cv2.VideoWriter(videolocation, 
+                         cv2.VideoWriter_fourcc(*'mp4v'),
+                        fps, size)
+
     while (video.isOpened()):
         print("recording video here")
         ret, frame = video.read()
         if not ret:
             output.release()
         output.write(frame)
+    
+
+def record(request):
+    now = datetime.now()
+    current_time = now.strftime("%H-%M-%S")
+    global videoname
+    videoname = os.environ.get('VIDEO_FILENAME')  + current_time+".mp4"
+    global videolocation
+    videolocation = os.environ.get('VIDEO_PATH') + videoname
+    thread = threading.Thread(target=background_record(request = request, videolocation=videolocation))
+    thread.start()
     return HttpResponse("Started Recording")
 
 def stoprecord(request):
-    video.release()
+    global video
     output.release()
-    found = client.bucket_exists("raspi-bucket")
+    video.release()
+    found = client.bucket_exists(os.environ.get('MINIO_BUCKET_NAME'))
     print('Starting Upload')
     if not found:
         return HttpResponse("Issue with uploading. Bucket not found")
-    now = datetime.now()
-    current_time = now.strftime("%H-%M-%S")
-    print(videolocation)
+
+    global videolocation
+    global videoname
+
     client.fput_object(
-        "raspi-bucket", "record" + current_time+".mp4", videolocation,
+        os.environ.get('MINIO_BUCKET_NAME'), videoname, videolocation,
     )
+
     print('Finished Uploading')
-    return HttpResponse("Stopped recording")
+    return HttpResponse("Stopped recording uploaded video with name " + videoname +" to location "+ videolocation)
 
 def command(request, command):
     if command == "backward":
@@ -105,5 +129,6 @@ def command(request, command):
         GPIO.output(10, False)
     else:
         print(command)
+        GPIO.cleanup()
         return HttpResponse("This Command doesn't exist. Please choose from forward/backward/right/left/stop")
     return HttpResponse("Your " + command +" Command has been executed")
