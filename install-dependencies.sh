5c0f0fbcd031b53806976ada203eae8f125f5116
bala siddhartha <bala.siddhartha@gmail.com>
2023-08-02 16:57:02 +0530

    Fixing dependency issues

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
-apt-get -y update
+apt-get -y update --fix-missing
 apt-get -y  upgrade
-apt-get install -y python3 cmake python3-pip python3-django python3-dev python3-opencv
+apt-get install -y python3 cmake python3-pip python3-django python3-dev python3-opencv libatlas3-base libsz2 libharfbuzz0b libwebp6 libtiff5
 dpkg --configure -a
 apt autoremove -y
 pip install -r requirements.txt
